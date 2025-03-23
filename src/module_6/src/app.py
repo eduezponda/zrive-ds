bcf59188fbd48f83101b9142908a7c5a051cf805
ezponda.147875 <ezponda.147875@e.unavarra.es>
2025-03-23 14:51:12 +0200

    Change directory of app.py and eliminate sys.path.append()

--- src/module_6/src/app.py
+++ src/module_6/src/app.py
@@ -1,11 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-
-from routers import status, predict
+from basket_model.routers import status, predict
 
 app = FastAPI()
 
