a1d150696725676f64f3475d83fe9eb0969f598e
ezponda.147875 <ezponda.147875@e.unavarra.es>
2025-03-23 14:51:53 +0200

    Add dependencies for endpoint predict

---
+++ src/module_6/src/basket_model/dependencies/predict.py
@@ -0,0 +1,6 @@
+from basket_model.services.feature_store import FeatureStore
+from basket_model.services.basket_model import BasketModel
+
+
+feature_store = FeatureStore()
+basket_model = BasketModel()
